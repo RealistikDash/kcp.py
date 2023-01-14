27e80245a0ca1c01124260b491afa1a7a33dc81f
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2023-01-14 21:23:08 +0000

    add: typing information

---
+++ kcp/extension.pyi
@@ -0,0 +1,31 @@
+from typing import Optional
+
+class KCPControl:
+    def __init__(self) -> None: ...
+
+    def send(self, data: bytes) -> None: 
+        """Enqueues data to be sent to the remote peer."""
+        ...
+
+    def receive(self, data: bytes) -> None:
+        """Enqueues data to be received from the remote peer."""
+        ...
+
+    def update(self, ts_ms: Optional[int] = None) -> Optional[None]:
+        """Updates the connection timing information. May return a packet to be sent to the remote peer.
+        
+        If `ts_ms` is not provided, it will be set to the current time in milliseconds computed by the Python interpreter.
+        """
+        ...
+    
+    def get_queued_packets(self) -> int:
+        """Returns the number of packets queued to be sent to the remote peer."""
+        ...
+
+    def read_outbound(self) -> bytes:
+        """Outputs the KCP packet data to be sent to the remote peer."""
+        ...
+
+    def read_inbound(self) -> bytes:
+        """Outputs the data received from the remote peer."""
+        ...
