bb158f3cb53352db62ef25f03e7e01df801d295f
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2023-01-24 23:34:16 +0000

    bump version

--- kcp/__init__.py
+++ kcp/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 __description__ = "Python bindings and abstarctions over the KCP protocol."
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "RealistikDash <realistikdash@gmail.com>"
 __license__ = "MIT"
 
