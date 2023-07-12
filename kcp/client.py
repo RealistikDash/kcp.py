3099f62ec8f83b9fd831e8253f4cce1bc6e6c016
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2023-07-12 19:28:32 +0100

    add: performance settings to client

--- kcp/client.py
+++ kcp/client.py
@@ -12,13 +12,45 @@ SyncDataHandler = Callable[[bytes], None]
 
 
 class KCPClientSync:
-    def __init__(self, address: str, port: int, conv_id: int) -> None:
-        """Configures a synchrnous KCP client."""
+    def __init__(
+        self,
+        address: str,
+        port: int,
+        conv_id: int,
+        no_delay: bool,
+        update_interval: int,
+        resend_count: int,
+        no_congestion_control: bool,
+        receive_window_size: int,
+        send_window_size: int,
+    ) -> None:
+        """Configures a synchronous KCP client.
+
+        :param address: The address of the target server.
+        :param port: The port of the target server.
+        :param conv_id: The conversation ID. Must be equal on both ends of the
+            connection.
+        :param no_delay: Whether to enable no-delay mode.
+        :param update_interval: The internal update interval in milliseconds.
+        :param resend_count: The number of times to resend a packet if it is
+            not acknowledged.
+        :param no_congestion_control: Whether to disable congestion control.
+        :param receive_window_size: The size of the receive window in packets.
+        :param send_window_size: The size of the send window in packets.
+        """
 
         self.address = address
         self.port = port
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-        self._kcp = KCP(conv_id)
+        self._kcp = KCP(
+            conv_id,
+            no_delay=no_delay,
+            update_interval=update_interval,
+            resend_count=resend_count,
+            no_congestion_control=no_congestion_control,
+            receive_window_size=receive_window_size,
+            send_window_size=send_window_size,
+        )
         self._kcp.include_outbound_handler(self._send_kcp)
         # Port 0 means the OS will pick a random port.
         self._local_port = 0
