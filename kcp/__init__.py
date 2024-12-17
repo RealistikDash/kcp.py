13613d72cab5e6cd1b64e847befa245fe1bad513
RealistikDash <realistikdash@gmail.com>
2024-12-17 17:48:03 +0000

    [0.1.6] pre-build more binaries

--- kcp/__init__.py
+++ kcp/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 __description__ = "Python bindings and abstarctions over the KCP protocol."
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__ = "RealistikDash <realistikdash@gmail.com>"
 __license__ = "MIT"
 
